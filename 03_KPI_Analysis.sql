f226cfc618eaf325ba9c9112d324753802842a1e
Sudip-Dalai <sudipvdalai@gmail.com>
2026-02-12 19:42:29 +0530

    Remove average pizza per day calculation query
    Removed the query that calculates the average number of pizzas ordered per day.

--- 03_KPI_Analysis.sql
+++ 03_KPI_Analysis.sql
@@ -30,18 +30,3 @@ FROM
     order_details ON order_details.pizza_id = pizzas.pizza_id
 GROUP BY pizza_types.category
 ORDER BY quantity DESC;
-
-
-
-
--- Group the orders by date and calculate the average number of pizza ordered per day
-
-SELECT 
-    ROUND(AVG(quantity), 0) as avg_pizza_per_day
-FROM
-    (SELECT 
-        orders.order_date, SUM(order_details.quantity) AS quantity
-    FROM
-        orders
-    JOIN order_details ON orders.order_id = order_details.order_id
-    GROUP BY orders.order_date) AS order_quantity;  
